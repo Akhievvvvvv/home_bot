6364cdf253821ec694d1deb74652bb1dfa9d36de
Akhievvvvvv <ahievamir5@gmail.com>
2025-08-30 14:34:01 +0300

    Update payments.py

--- bot/utils/payments.py
+++ bot/utils/payments.py
@@ -1,14 +1,16 @@
 from aiogram import types
+from aiogram.types import LabeledPrice
+from bot.config.settings import PROVIDER_TOKEN
 
 async def send_stars_invoice(message: types.Message, plan_name: str, price: int):
-    prices = [types.LabeledPrice(label=plan_name, amount=price*100)]
+    prices = [LabeledPrice(label=plan_name, amount=price*100)]
     await message.bot.send_invoice(
         chat_id=message.chat.id,
-        title=f"Подписка VPN - {plan_name}",
-        description=f"VPN доступ на {plan_name}",
-        payload="vpn_subscription",
-        provider_token="",
-        currency="XTR",
+        title=f"VPN: {plan_name}",
+        description=f"Оплата тарифа {plan_name} через ⭐️Stars",
+        payload=f"plan_{plan_name}",
+        provider_token=PROVIDER_TOKEN,
+        currency="RUB",
         prices=prices,
-        start_parameter="vpn-stars-payment"
+        start_parameter="vpn_payment"
     )
