11c5d3a5b0142e62bcaa06465648c5bf93d59427
Akhievvvvvv <ahievamir5@gmail.com>
2025-08-30 14:58:00 +0300

    Update settings.py

--- bot/config/settings.py
+++ bot/config/settings.py
@@ -1,12 +1,10 @@
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x]
-DEFAULT_LANGUAGE = os.getenv("DEFAULT_LANGUAGE", "ru")
+ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS","").split(",")]
 
-VPN_IPV4 = os.getenv("VPN_IPV4")
-VPN_IPV6 = os.getenv("VPN_IPV6")
-PROVIDER_TOKEN = os.getenv("PROVIDER_TOKEN")
+# Настройки VPN (для будущей интеграции)
+VPN_SERVER = os.getenv("VPN_SERVER_URL", "5.129.197.99")
+VPN_API_KEY = os.getenv("VPN_API_KEY", "")
