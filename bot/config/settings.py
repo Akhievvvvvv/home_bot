5cbfcde36ad3750460f70566b825c2718d8a58f0
Akhievvvvvv <ahievamir5@gmail.com>
2025-08-31 12:09:00 +0300

    Update settings.py

--- bot/config/settings.py
+++ bot/config/settings.py
@@ -1,10 +1,24 @@
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS","").split(",")]
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
 
-# Настройки VPN (для будущей интеграции)
-VPN_SERVER = os.getenv("VPN_SERVER_URL", "5.129.197.99")
-VPN_API_KEY = os.getenv("VPN_API_KEY", "")
+# Outline VPN
+OUTLINE_API_URL = os.getenv("OUTLINE_API_URL")
+OUTLINE_CERT_SHA256 = os.getenv("OUTLINE_CERT_SHA256")
+
+VPN_IPV4 = os.getenv("VPN_IPV4")
+VPN_IPV6 = os.getenv("VPN_IPV6")
+
+# Тарифы (звёзды)
+PLAN_1_MONTH = int(os.getenv("PLAN_1_MONTH", 49))
+PLAN_2_MONTH = int(os.getenv("PLAN_2_MONTH", 89))
+PLAN_3_MONTH = int(os.getenv("PLAN_3_MONTH", 119))
+
+# Реферальная система
+REFERRAL_BONUS_PERCENT = int(os.getenv("REFERRAL_BONUS_PERCENT", 10))
+REFERRAL_MIN_PAYOUT = int(os.getenv("REFERRAL_MIN_PAYOUT", 100))
