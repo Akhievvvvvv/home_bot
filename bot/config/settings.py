f34f835878cfca85f78b328f107d174c3e54a456
Akhievvvvvv <ahievamir5@gmail.com>
2025-09-03 20:01:15 +0300

    Update settings.py

--- bot/config/settings.py
+++ bot/config/settings.py
@@ -1,8 +1,27 @@
-BOT_TOKEN = "7731916113:AAGa1CZ47eEG1qz3gQOJvsR6_7ELvDJddZg"
-ADMIN_ID = 7231676236
-LOG_LEVEL = "INFO"
+import os
+from dotenv import load_dotenv
 
-# Серверные данные для VPN
-VPN_IPV4 = "5.129.197.99"
-VPN_ROOT_PASSWORD = "hP9CsxoHLUnq,3"
-VPN_CLIENTS_DIR = "/root/home_bot/clients"
+# Загружаем .env
+load_dotenv()
+
+# Токен и админы
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "7231676236").split(",")))
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+
+# VPN
+VPN_IPV4 = os.getenv("VPN_IPV4", "5.129.197.99")
+VPN_ROOT_PASSWORD = os.getenv("VPN_ROOT_PASSWORD", "hP9CsxoHLUnq,3")
+VPN_CLIENTS_DIR = os.getenv("VPN_CLIENTS_DIR", "/root/home_bot/clients")
+
+# Тарифы
+PLAN_1_MONTH = int(os.getenv("PLAN_1_MONTH", 79))
+PLAN_2_MONTH = int(os.getenv("PLAN_2_MONTH", 129))
+PLAN_3_MONTH = int(os.getenv("PLAN_3_MONTH", 149))
+
+# Рефералы
+REFERRAL_BONUS_PERCENT = int(os.getenv("REFERRAL_BONUS_PERCENT", 10))
+REFERRAL_MIN_PAYOUT = int(os.getenv("REFERRAL_MIN_PAYOUT", 50))
+
+# Язык
+LANG = 'ru'
